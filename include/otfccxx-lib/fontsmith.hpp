ad4fef7f78777d20f9f3b5b2121c874297e42738
Michal Lurie <Michal.Lurie@gmail.com>
2025-12-11 01:21:33 +0100

    Feat: developing subsetter pt.2

--- include/otfccxx-lib/fontsmith.hpp
+++ include/otfccxx-lib/fontsmith.hpp
@@ -1,41 +1,85 @@
 #pragma once
 
-
-#include <algorithm>
-#include <cmath>
-#include <cstddef>
-#include <cstdlib>
+#include "hb-subset.h"
+#include "hb.h"
 #include <expected>
-#include <string>
-#include <unordered_set>
-#include <utility>
-#include <vector>
+#include <filesystem>
+
+#include <optional>
+#include <span>
+
 
 #include <hb-subset.hh>
-#include <nlohmann/json.hpp>
+// #include <nlohmann/json.hpp>
 #include <otfcc/font.h>
 #include <otfcc/sfnt.h>
 
 
 namespace fontsmith {
 
-class Subsetter {
-    using faceUPtr = std::unique_ptr<hb_face_t, decltype([](hb_face_t *f) noexcept {
+enum class err : size_t {
+    uknownError = 1,
+    hb_blob_t_createFailure,
+    hb_face_t_createFailure,
+    execute_someRequestedGlyphsAreMissing,
+    subsetInput_failedToCreate,
+    hb_subset_executeFailure,
+    make_subset_noIntersectingGlyphs,
+    unexpectedNullptr,
+};
+using hb_face_uptr = std::unique_ptr<hb_face_t, decltype([](hb_face_t *f) noexcept {
                                          if (f) { hb_face_destroy(f); }
                                      })>;
+using hb_blob_uptr = std::unique_ptr<hb_blob_t, decltype([](hb_blob_t *b) noexcept {
+                                         if (b) { hb_blob_destroy(b); }
+                                     })>;
 
+using hb_set_uptr = std::unique_ptr<hb_set_t, decltype([](hb_set_t *s) noexcept {
+                                        if (s) { hb_set_destroy(s); }
+                                    })>;
+
+using hb_subset_input_uptr = std::unique_ptr<hb_subset_input_t, decltype([](hb_subset_input_t *si) noexcept {
+                                                 if (si) { hb_subset_input_destroy(si); }
+                                             })>;
+
+class Subsetter {
 
 public:
-    Subsetter &add_ff_toSubset(std::span<const char> buf);
-    Subsetter &add_ff_categoryBackup(std::span<const char> buf);
-    Subsetter &add_ff_lastResort(std::span<const char> buf);
+    Subsetter &add_ff_toSubset(std::span<const char> buf, unsigned int const faceIndex = 0u);
+    Subsetter &add_ff_categoryBackup(std::span<const char> buf, unsigned int const faceIndex = 0u);
+    Subsetter &add_ff_lastResort(std::span<const char> buf, unsigned int const faceIndex = 0u);
+
+    Subsetter &add_ff_toSubset(std::filesystem::path const &pth, unsigned int const faceIndex = 0u);
+    Subsetter &add_ff_categoryBackup(std::filesystem::path const &pth, unsigned int const faceIndex = 0u);
+    Subsetter &add_ff_lastResort(std::filesystem::path const &pth, unsigned int const faceIndex = 0u);
+
+
+    Subsetter &add_toKeep_CP(hb_codepoint_t cp);
+    Subsetter &add_toKeep_CPs(std::span<const hb_codepoint_t> cps);
+
+
+    std::expected<std::vector<hb_face_uptr>, err>                         execute();
+    std::expected<std::pair<std::vector<hb_face_uptr>, hb_set_uptr>, err> execute_bestEffort();
+
+
+    bool is_inError() { return inError.has_value(); }
+    err  get_error() { return inError.value(); }
+
 
 private:
-    std::vector<faceUPtr> ffs_toSubset;
+    std::expected<hb_face_uptr, err> make_ff(std::span<const char> const &buf, unsigned int const faceIndex);
 
-    std::vector<faceUPtr> ffs_categoryBackup;
+    std::expected<hb_face_uptr, err> make_subset(hb_face_t *ff);
+    std::expected<bool, err>         should_include_category(hb_face_t *ff);
 
-    std::vector<faceUPtr> ffs_lastResort;
+    hb_set_uptr toKeep_unicodeCPs;
+
+    std::vector<hb_face_uptr> ffs_toSubset;
+    std::vector<hb_face_uptr> ffs_categoryBackup;
+    std::vector<hb_face_uptr> ffs_lastResort;
+
+
+    std::optional<err> inError = std::nullopt;
 };
 
 } // namespace fontsmith
---
+++ src/fontsmith.cpp
@@ -0,0 +1,151 @@
+#pragma once
+
+#include <expected>
+#include <fstream>
+
+
+#include <otfccxx-lib/fontsmith.hpp>
+#include <utility>
+
+
+namespace fontsmith {
+
+// Adding FontFaces
+Subsetter &Subsetter::add_ff_toSubset(std::span<const char> buf, unsigned int const faceIndex) {
+    if (auto toInsert = make_ff(buf, faceIndex); toInsert.has_value()) {
+        ffs_toSubset.push_back(std::move(toInsert.value()));
+    }
+    return *this;
+}
+Subsetter &Subsetter::add_ff_categoryBackup(std::span<const char> buf, unsigned int const faceIndex) {
+    if (auto toInsert = make_ff(buf, faceIndex); toInsert.has_value()) {
+        ffs_categoryBackup.push_back(std::move(toInsert.value()));
+    }
+    return *this;
+}
+Subsetter &Subsetter::add_ff_lastResort(std::span<const char> buf, unsigned int const faceIndex) {
+    if (auto toInsert = make_ff(buf, faceIndex); toInsert.has_value()) {
+        ffs_lastResort.push_back(std::move(toInsert.value()));
+    }
+    return *this;
+}
+
+// Adding unicode character points and/or glyph IDs
+
+Subsetter &Subsetter::add_toKeep_CP(hb_codepoint_t const cp) {
+    hb_set_add(toKeep_unicodeCPs.get(), cp);
+    return *this;
+}
+
+Subsetter &Subsetter::add_toKeep_CPs(std::span<const hb_codepoint_t> const cps) {
+    for (auto &cp : cps) { hb_set_add(toKeep_unicodeCPs.get(), cp); }
+    return *this;
+}
+
+
+// Execution
+
+std::expected<std::vector<hb_face_uptr>, err> Subsetter::execute() {
+    if (auto res = execute_bestEffort(); res.has_value()) {
+        if (hb_set_is_empty(res.value().second.get())) { return std::move(res.value().first); }
+        else { return std::unexpected(err::execute_someRequestedGlyphsAreMissing); }
+    }
+    else { return std::unexpected(res.error()); }
+}
+
+
+std::expected<std::pair<std::vector<hb_face_uptr>, hb_set_uptr>, err> Subsetter::execute_bestEffort() {
+    std::vector<hb_face_uptr> res;
+
+    for (auto &ff_to : ffs_toSubset) {
+        if (hb_set_is_empty(toKeep_unicodeCPs.get())) { goto RET; }
+
+        auto exp_ff = make_subset(ff_to.get());
+        if (not exp_ff.has_value()) {
+            if (exp_ff.error() == err::make_subset_noIntersectingGlyphs) { continue; }
+            else { return std::unexpected(exp_ff.error()); }
+        }
+        else { res.push_back(std::move(exp_ff.value())); }
+    }
+
+    for (auto &ff_to : ffs_categoryBackup) {
+        if (hb_set_is_empty(toKeep_unicodeCPs.get())) { goto RET; }
+
+        auto exp_ff = should_include_category(ff_to.get());
+        if (not exp_ff.has_value()) {
+            if (exp_ff.error() == err::make_subset_noIntersectingGlyphs) { continue; }
+            else { return std::unexpected(exp_ff.error()); }
+        }
+        else { res.push_back(hb_face_uptr(hb_face_reference(ff_to.get()))); }
+    }
+
+    for (auto &ff_to : ffs_lastResort) {
+        if (hb_set_is_empty(toKeep_unicodeCPs.get())) { goto RET; }
+
+        auto exp_ff = make_subset(ff_to.get());
+        if (not exp_ff.has_value()) {
+            if (exp_ff.error() == err::make_subset_noIntersectingGlyphs) { continue; }
+            else { return std::unexpected(exp_ff.error()); }
+        }
+        else { res.push_back(std::move(exp_ff.value())); }
+    }
+
+RET:
+    return std::make_pair(std::move(res), hb_set_uptr(hb_set_copy(toKeep_unicodeCPs.get())));
+}
+
+
+// PRIVATE METHODS
+std::expected<hb_face_uptr, err> Subsetter::make_ff(std::span<const char> const &buf, unsigned int const faceIndex) {
+    hb_blob_uptr blob(hb_blob_create_or_fail(buf.data(), buf.size_bytes(), HB_MEMORY_MODE_READONLY, nullptr, nullptr));
+    if (! blob) { return std::unexpected(err::hb_blob_t_createFailure); }
+
+    // Create face from blob
+    hb_face_uptr face(hb_face_create_or_fail(blob.get(), faceIndex));
+    if (! face) { return std::unexpected(err::hb_face_t_createFailure); }
+
+    return face;
+}
+
+
+std::expected<hb_face_uptr, err> Subsetter::make_subset(hb_face_t *ff) {
+    hb_set_uptr unicodes_toKeep_in_ff(hb_set_create());
+    hb_face_collect_unicodes(ff, unicodes_toKeep_in_ff.get());
+
+    hb_set_intersect(unicodes_toKeep_in_ff.get(), toKeep_unicodeCPs.get());
+    if (hb_set_is_empty(unicodes_toKeep_in_ff.get())) { return std::unexpected(err::make_subset_noIntersectingGlyphs); }
+
+    // Set the unicodes to keep in the subsetted font
+    hb_subset_input_uptr si(hb_subset_input_create_or_fail());
+    if (! si) { return std::unexpected(err::subsetInput_failedToCreate); }
+
+    hb_set_t *si_inputUCCPs = hb_subset_input_unicode_set(si.get());
+    hb_set_set(si_inputUCCPs, unicodes_toKeep_in_ff.get());
+
+    // Set subsetting flags
+    hb_subset_input_set_flags(si.get(), HB_SUBSET_FLAGS_DEFAULT);
+
+    // Execute subsetting
+    hb_face_uptr res(hb_subset_or_fail(ff, si.get()));
+    if (! res) { return std::unexpected(err::hb_subset_executeFailure); }
+
+    // Only keep the remaining unicodeCPs by 'filtering' the ones we use from 'ff'
+    hb_set_symmetric_difference(toKeep_unicodeCPs.get(), unicodes_toKeep_in_ff.get());
+    return res;
+}
+
+std::expected<bool, err> Subsetter::should_include_category(hb_face_t *ff) {
+    hb_set_uptr unicodes_toKeep_in_ff(hb_set_create());
+    hb_face_collect_unicodes(ff, unicodes_toKeep_in_ff.get());
+
+    hb_set_intersect(unicodes_toKeep_in_ff.get(), toKeep_unicodeCPs.get());
+
+    bool res = not hb_set_is_empty(unicodes_toKeep_in_ff.get());
+
+    // Only keep the remaining unicodeCPs by 'filtering' the ones we use from 'ff'
+    hb_set_symmetric_difference(toKeep_unicodeCPs.get(), unicodes_toKeep_in_ff.get());
+    return res;
+}
+
+
+} // namespace fontsmith
